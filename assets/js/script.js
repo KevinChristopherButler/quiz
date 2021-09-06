5fa83ef9f40dc7f8f69144ab37bd852a8ea36af3
Kevin Butler <kcbutler@eircom.net>
2021-09-06 21:21:23 +0100

    Extend quiz dictionary, add username processing

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
             if (this.getAttribute("data-type") === "nextQuestion") {
                 displayNextQuestion();
             } else if (this.getAttribute("data-type") === "submitAnswer") {
-		checkAnswer()
+                checkAnswer()
             } else {
                 let quizType = this.getAttribute("data-type");
                 runQuiz(quizType);
@@ -33,8 +33,30 @@ function runQuiz(quizType) {
     displayNextQuestion(quizType, 0);
 }
 
+function getUserName() {
+    alert("getUserName");
+    let userName = document.getElementById("userName").value;
+    userName = (userName === "")? "Anonymous" : userName;
+    alert("I think the userName is " + userName);
+    return userName;
+}
+
 function checkAnswer() {
-    alert("Checking the answer 1.");
+    alert("Checking the answer.");
+    let response;
+
+    /* Disable the radio button group. */
+    let radioButtons = document.getElementsByName("possibleAnswer");
+    for (radioButton of radioButtons) {
+        radioButton.disabled = true;
+    }
+
+    /* Disable the Submit Answer button. */
+    document.getElementById("submitAnswer").disabled = true;
+
+    /* Enable the Next Question button. */
+    document.getElementById("nextQuestion").disabled = false;
+
     let userAnswer = "XXX";
     let quizType = determineCurrentQuiz();
     let lastQuestionNumber = determineCurrentQuestion();
@@ -47,58 +69,84 @@ function checkAnswer() {
             break;
         }
     }
-    alert("The user answer is " + userAnswer);
-    alert("The correct answer is " + quizzes[quizType][lastQuestionNumberIndex].rightAnswer );
     if (userAnswer === quizzes[quizType][lastQuestionNumberIndex].rightAnswer) {
-        alert("Answer is correct!");
         updateScoreboard(true);
+        response = "Correct!";
     }
     else {
-        alert("Answer is incorrect!");
         updateScoreboard(false);
+        response = "I'm afraid not, " + getUserName() + " - the correct answer is \"" + quizzes[quizType][lastQuestionNumberIndex].rightAnswer + "\".";
     }
-    return false;
+    /* Display the response on screen. */
+    document.getElementById("answerAndFeedback").innerHTML = "<p>" + response + "</p>";
 }
 
+/**
+ * Display the next question - if there is one!
+ * If there are no more questions for the current quiz category, format an
+ * appropriate message for the user.
+ */
 function displayNextQuestion() {
-	alert("Displaying the next question.");
-	let quizType = determineCurrentQuiz();
-	let lastQuestionNumber = determineCurrentQuestion();
-        alert("displayNextQuestion: " + quizType + " " + lastQuestionNumber);
-	let nextQuestionNumber = parseInt(lastQuestionNumber) + 1;
-	document.getElementById("questionNumber").innerText = "Question " + nextQuestionNumber.toString();
-	let questionNumber = nextQuestionNumber - 1;
+    alert("Displaying the next question.");
 
-        let questionHTML = `<p>${quizzes[quizType][questionNumber].question}<p>
-                            <input type="radio" id="option0" name="possibleAnswer" value="${quizzes[quizType][questionNumber].suggestedAnswers[0]}" required>
-                            <label for="option0">${quizzes[quizType][questionNumber].suggestedAnswers[0]}</label><br>
-                            <input type="radio" id="option1" name="possibleAnswer" value="${quizzes[quizType][questionNumber].suggestedAnswers[1]}">
-                            <label for="option1">${quizzes[quizType][questionNumber].suggestedAnswers[1]}</label><br>
-                            <input type="radio" id="option2" name="possibleAnswer" value="${quizzes[quizType][questionNumber].suggestedAnswers[2]}">
-                            <label for="option2">${quizzes[quizType][questionNumber].suggestedAnswers[2]}</label><br>`;
-	document.getElementById("displayQuestion").innerHTML = questionHTML;
-	alert(questionHTML);
+    /* Blank out the last response.      */
+    document.getElementById("answerAndFeedback").innerHTML = "<p></p>";
+
+    /* Disable the Next Question button. */
+    document.getElementById("nextQuestion").disabled = true;
+
+    /* Disable the Submit Answer button. This will be enabled when an answer radio button is clicked. */
+    document.getElementById("submitAnswer").disabled = true;
+
+    /* Determine the quiz category and the number of the last question displayed within
+       that category. */
+    let quizType = determineCurrentQuiz();
+    let lastQuestionNumber = determineCurrentQuestion();
+
+    /* If the last question displayed is the last question in the category, format a suitable
+       message. */
+    if (parseInt(lastQuestionNumber) < quizzes[quizType].length) {
+        let nextQuestionNumber = parseInt(lastQuestionNumber) + 1;
+        document.getElementById("questionNumber").innerText = "Question " + nextQuestionNumber.toString();
+        let questionNumber = nextQuestionNumber - 1;
+
+        let questionHTML =
+            `<p>${quizzes[quizType][questionNumber].question}<p>
+             <input type="radio" id="option0" name="possibleAnswer" value="${quizzes[quizType][questionNumber].suggestedAnswers[0]}" required>
+             <label for="option0">${quizzes[quizType][questionNumber].suggestedAnswers[0]}</label><br>
+             <input type="radio" id="option1" name="possibleAnswer" value="${quizzes[quizType][questionNumber].suggestedAnswers[1]}" required>
+             <label for="option1">${quizzes[quizType][questionNumber].suggestedAnswers[1]}</label><br>
+             <input type="radio" id="option2" name="possibleAnswer" value="${quizzes[quizType][questionNumber].suggestedAnswers[2]}" required>
+             <label for="option2">${quizzes[quizType][questionNumber].suggestedAnswers[2]}</label><br>`;
+        document.getElementById("displayQuestion").innerHTML = questionHTML;
+        /* Add a click event listener to each radio button so that the Submit Answer button may be enabled. */ 
+        radioButtons = document.getElementsByName("possibleAnswer");
+        for (let i = 0; i < radioButtons.length; i++) {
+            radioButtons[i].onclick = function() { document.getElementById("submitAnswer").disabled = false; }
+        }
+    }
+    else {
+        let response = "Sorry, " + getUserName() + "! There are no more questions in this quiz. Why not try another category?";
+        /* Display the response on screen. */
+        document.getElementById("answerAndFeedback").innerHTML = "<p>" + response + "</p>";
+    }
+
 }
 
 function updateScoreboard(correctAnswer) {
     alert("updateScoreboard");
     if (correctAnswer) {
-        alert("updateScoreboard: correct start");
         let oldThisCorrectScore = parseInt(document.getElementById("thisCorrectScore").innerText);
         document.getElementById("thisCorrectScore").innerText = ++oldThisCorrectScore;
         let oldAllCorrectScore = parseInt(document.getElementById("allCorrectScore").innerText);
         document.getElementById("allCorrectScore").innerText = ++oldAllCorrectScore;
-        alert("updateScoreboard: correct end");
     }
     else {
-        alert("updateScoreboard: incorrect start");
         let oldThisIncorrectScore = parseInt(document.getElementById("thisIncorrectScore").innerText);
         document.getElementById("thisIncorrectScore").innerText = ++oldThisIncorrectScore;
         let oldAllIncorrectScore = parseInt(document.getElementById("allIncorrectScore").innerText);
         document.getElementById("allIncorrectScore").innerText = ++oldAllIncorrectScore;
-        alert("updateScoreboard: incorrect end");
     }
-    return false;
 }
 
 /**
@@ -118,16 +166,24 @@ function determineCurrentQuestion() {
 }
 
 const quizzes = {
-	football   : [{question: "F Who?", suggestedAnswers: ["Tom", "Dick", "Harry"], rightAnswer: "Dick"},
-                      {question: "F Where?", suggestedAnswers: ["Peru", "Japan", "Korea"], rightAnswer: "Peru"},
-                      {question: "F Why?", suggestedAnswers: ["Snow", "Crowd", "War"], rightAnswer: "Snow"}],
-        history    : [{question: "H Who?", suggestedAnswers: ["Tom", "Dick", "Harry"], rightAnswer: "Dick"},
-                      {question: "H Where?", suggestedAnswers: ["Peru", "Japan", "Korea"], rightAnswer: "Peru"},
-                      {question: "H Why?", suggestedAnswers: ["Snow", "Crowd", "War"], rightAnswer: "Snow"}],
-        literature : [{question: "L Who?", suggestedAnswers: ["Tom", "Dick", "Harry"], rightAnswer: "Dick"},
-                      {question: "L Where?", suggestedAnswers: ["Peru", "Japan", "Korea"], rightAnswer: "Peru"},
-                      {question: "L Why?", suggestedAnswers: ["Snow", "Crowd", "War"], rightAnswer: "Snow"}],
-        science    : [{question: "S Who?", suggestedAnswers: ["Tom", "Dick", "Harry"], rightAnswer: "Dick"},
-                      {question: "S Where?", suggestedAnswers: ["Peru", "Japan", "Korea"], rightAnswer: "Peru"},
-                      {question: "S Why?", suggestedAnswers: ["Snow", "Crowd", "War"], rightAnswer: "Snow"}],
+	football   : [{question: "Paul McGrath played for which club in 1990?", suggestedAnswers: ["Manchester United", "St. Patrick's Athletic", "Aston Villa"], rightAnswer: "Aston Villa"},
+                      {question: "Which team won the first FA Cup Final played in the old Wembley Stadium?", suggestedAnswers: ["Bolton Wanderers", "West Ham", "Arsenal"], rightAnswer: "Bolton Wanderers"},
+                      {question: "In which season were substitutes first allowed in English League football?", suggestedAnswers: ["1959-60", "1965-66", "1971-72"], rightAnswer: "1965-66"},
+                      {question: "Who was the leading scorer at the 1970 World Cup finals?", suggestedAnswers: ["Pele", "Gerd Muller", "Bobby Charlton"], rightAnswer: "Gerd Muller"},
+                      {question: "Which country hosted the World Cup finals in 1962?", suggestedAnswers: ["Uruguay", "Sweden", "Chile"], rightAnswer: "Chile"}],
+        history    : [{question: "Lev Davidovich Bronstein was the real name of which historical person?", suggestedAnswers: ["Stalin", "Rasputin", "Trotsky"], rightAnswer: "Trotsky"},
+                      {question: "On what date in 1941 did the Japanese attack on Pearl Harbour occur?", suggestedAnswers: ["5th January", "6th June", "7th December"], rightAnswer: "7th December"},
+                      {question: "Who was the Prime Minister of Britain during the Abdication Crisis in 1936?", suggestedAnswers: ["Stanley Baldwin", "Neville Chamberlain", "Winston Churchill"], rightAnswer: "Stanley Baldwin"},
+                      {question: "Which French monarch was known as The Sun King?", suggestedAnswers: ["Charles IV", "Louis XIV", "Louis XVI"], rightAnswer: "Louis XIV"},
+                      {question: "John F. Kennedy succeeded whom as President of the USA?", suggestedAnswers: ["Harry S. Truman", "Dwight D. Eisenhower", "Lyndon B. Johnson"], rightAnswer: "Dwight D. Eisenhower"}],
+        literature : [{question: "Eric Blair was the real name of which author?", suggestedAnswers: ["Nevil Shute", "George Orwell", "Somerset Maugham"], rightAnswer: "George Orwell"},
+                      {question: "Who won the Nobel Prize for Literature in 1953?", suggestedAnswers: ["Ernest Hemingway", "Jean Paul Sartre", "Winston Churchill"], rightAnswer: "Winston Churchill"},
+                      {question: "Who is the author of the novel Brighton Rock?", suggestedAnswers: ["Aldous Huxley", "Gore Vidal", "Graham Greene"], rightAnswer: "Graham Greene"},
+                      {question: "Which English poet died in Venice on 12th December, 1889?", suggestedAnswers: ["Alfred Lord Tennyson", "John Keats", "Robert Browning"], rightAnswer: "Robert Browning"},
+                      {question: "In which Charles Dickens book does Little Nell appear?", suggestedAnswers: ["The Old Curiosity Shop", "Bleak House", "Oliver Twist"], rightAnswer: "The Old Curiosity Shop"}],
+        science    : [{question: "What would a scientist keep in a Leyden Jar?", suggestedAnswers: ["Acids", "Electrical Charge", "Anti-matter"], rightAnswer: "Electrical Charge"},
+                      {question: "Which of these elements is the heaviest?", suggestedAnswers: ["Carbon", "Nitrogen", "Oxygen"], rightAnswer: "Oxygen"},
+                      {question: "Ichthyology is the study of what?", suggestedAnswers: ["Fish", "Insects", "Bats"], rightAnswer: "Fish"},
+                      {question: "Basalt is an example of which type of rock?", suggestedAnswers: ["Metamorphic", "Igneous", "Sedimentary"], rightAnswer: "Igneous"},
+                      {question: "Crick and Watson specialised in which branch of science?", suggestedAnswers: ["Nuclear Physics", "Electromagnetism", "Genetics"], rightAnswer: "Genetics"}],
 }
